1b5f5fa1967de46bafdee78921ad8bff2dc72c2f
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-07-09 10:08:12 +0600

    server response

--- body.php
+++ body.php
@@ -2,6 +2,18 @@
     <body>
         <h1>Welcome to My Website</h1>
         <p>This is a simple webpage to demonstrate the structure of an HTML document.</p>
+        
+        <form action="server.php" method="post">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <br>
+            <button type="submit">Submit</button>
+        </form>
+        
+        
         <p>Feel free to explore the content and learn more about web development!</p>
     </body>
 </section>
---
+++ server.php
@@ -0,0 +1,3 @@
+<h1>
+    This is a simple PHP server file.
+</h1>
